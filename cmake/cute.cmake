30bc782e88e1e8240e63b0609db9162bd279f551
Piotr Usewicz <piotr@layer22.com>
2025-05-22 10:35:29 +0200

    Don't build shader comiler for now

--- cmake/cute.cmake
+++ cmake/cute.cmake
@@ -1,6 +1,7 @@
 set(CF_FRAMEWORK_BUILD_SAMPLES OFF)
 set(CF_FRAMEWORK_BUILD_TESTS OFF)
 set(CF_FRAMEWORK_STATIC OFF) # TODO: Set to ON for Release build
+set(CF_RUNTIME_SHADER_COMPILATION OFF)
 set(CF_CUTE_SHADERC OFF) # TODO: Do we want an offline shader compiler?
 
 if(APPLE)
