8067df88488835f40c6e868e865d57cb8cdde4a2
Piotr Usewicz <piotr@layer22.com>
2025-05-22 11:54:10 +0200

    Reformat cmake files

--- cmake/cute.cmake
+++ cmake/cute.cmake
@@ -5,14 +5,14 @@ set(CF_RUNTIME_SHADER_COMPILATION OFF)
 set(CF_CUTE_SHADERC OFF) # TODO: Do we want an offline shader compiler?
 
 if(APPLE)
-  set(CF_FRAMEWORK_APPLE_FRAMEWORK ON)
+    set(CF_FRAMEWORK_APPLE_FRAMEWORK ON)
 endif()
 
 include(FetchContent)
 FetchContent_Declare(
-  cute
-  GIT_REPOSITORY https://github.com/RandyGaul/cute_framework
-  GIT_TAG master
-  GIT_SHALLOW
+    cute
+    GIT_REPOSITORY https://github.com/RandyGaul/cute_framework
+    GIT_TAG master
+    GIT_SHALLOW
 )
 FetchContent_MakeAvailable(cute)
