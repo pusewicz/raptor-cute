ec21e0164d8581875d076e0798d2565d29081aef
Piotr Usewicz <piotr@layer22.com>
2025-06-04 10:47:25 +0200

    Enable online shader compilation

--- cmake/CuteFramework.cmake
+++ cmake/CuteFramework.cmake
@@ -1,8 +1,8 @@
 set(CF_FRAMEWORK_BUILD_SAMPLES OFF)
 set(CF_FRAMEWORK_BUILD_TESTS OFF)
 set(CF_FRAMEWORK_STATIC OFF) # TODO: Set to ON for Release build
-set(CF_RUNTIME_SHADER_COMPILATION OFF)
-set(CF_CUTE_SHADERC OFF) # TODO: Do we want an offline shader compiler?
+set(CF_RUNTIME_SHADER_COMPILATION ON)
+set(CF_CUTE_SHADERC ON) # TODO: Do we want an offline shader compiler?
 
 if(APPLE)
     set(CF_FRAMEWORK_APPLE_FRAMEWORK ON)
