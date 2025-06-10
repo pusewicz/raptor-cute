43daf41b04bb9f0a9c64f2c378d25707fdbeda62
Piotr Usewicz <piotr@layer22.com>
2025-06-10 11:47:33 +0200

    Show draw count

--- src/Raptor/GameplayScene.swift
+++ src/Raptor/GameplayScene.swift
@@ -396,7 +396,7 @@ class GameplayScene: Scene {
     withVaList([]) { args in
       igBegin("Hello", nil, 0)
       igTextV("Draw Count: \(cf_app_get_framerate())", args)
-      igTextV("Draw Count: \(drawCount)", args)
+      igTextV("Draw Count: \(game.drawCount)", args)
       igTextV("Player Position: \(state.player.position)", args)
       igTextV("Player Beams Count: \(state.playerBeams.count)", args)
       igTextV("Enemies Count: \(state.enemies.count)", args)
