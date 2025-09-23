1ea1482696f258f6b904ae99dee4f0ce4ca099e6
Piotr Usewicz <piotr@layer22.com>
2025-09-24 00:15:53 +0200

    Add initializer

--- src/Game/State.swift
+++ src/Game/State.swift
@@ -1,12 +1,19 @@
 import CCuteFramework
 
-struct State: Sendable {
-  var player: Player
+@MainActor
+class State {
   var playerBeams: [PlayerBeam] = []
   var enemies: [Enemy] = []
   var explosions: [Explosion] = []
   var debug = false
   var lives = 3
+  var player: Player
   var shader: CF_Shader
   var canvas: CF_Canvas
+
+  init(player: Player, shader: CF_Shader, canvas: CF_Canvas) {
+    self.player = player
+    self.shader = shader
+    self.canvas = canvas
+  }
 }
